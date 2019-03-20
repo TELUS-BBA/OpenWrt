f2b970ad812a2954bd3a876e397953e97af2a5c0
Vladimir Vid <vladimir.vid@sartura.hr>
2019-03-20 17:23:43 +0100

    uDPU TELUS patch
    * uncomment uDPU device target
    * add bootldr script
    * add backported mx25u12835f support
    
    Signed-off-by: Vladimir Vid <vladimir.vid@sartura.hr>

--- target/linux/mvebu/image/cortex-a53.mk
+++ target/linux/mvebu/image/cortex-a53.mk
@@ -26,23 +26,23 @@ define Device/armada-3720-db
 endef
 TARGET_DEVICES += armada-3720-db
 
-#define Device/uDPU
-#  KERNEL_NAME := Image
-#  KERNEL_LOADADDR := 0x00080000
-#  KERNEL := kernel-bin | install-dtb
-#  KERNEL_INITRAMFS_PREFIX := $$(IMG_PREFIX)-$$(DEVICE_NAME)-initramfs
-#  KERNEL_INITRAMFS_SUFFIX := .itb
-#  KERNEL_INITRAMFS := kernel-bin | gzip | fit gzip $$(DTS_DIR)/$$(DEVICE_DTS).dtb
-#  DEVICE_TITLE := Methode uDPU Board
-#  DEVICE_PACKAGES := f2fs-tools e2fsprogs fdisk ethtool kmod-usb2 kmod-usb3 kmod-e100 kmod-e1000 kmod-e1000e \
-#			kmod-igb kmod-ixgbevf kmod-mdio-gpio kmod-sky2 kmod-switch-mvsw61xx kmod-bonding kmod-eeprom-at24
-#  IMAGES := sysupgrade.tar.gz
-#  IMAGE_NAME := $$(IMAGE_PREFIX)-sysupgrade.tar.gz
-#  IMAGE/sysupgrade.tar.gz := uDPU-firmware | gzip | append-metadata
-#  DEVICE_DTS := uDPU
-#  DTS_DIR := $(DTS_DIR)/marvell
-#  SUPPORTED_DEVICES := methode,uDPU
-#endef
-#TARGET_DEVICES += uDPU
+define Device/uDPU
+  KERNEL_NAME := Image
+  KERNEL_LOADADDR := 0x00080000
+  KERNEL := kernel-bin | install-dtb
+  KERNEL_INITRAMFS_PREFIX := $$(IMG_PREFIX)-$$(DEVICE_NAME)-initramfs
+  KERNEL_INITRAMFS_SUFFIX := .itb
+  KERNEL_INITRAMFS := kernel-bin | gzip | fit gzip $$(DTS_DIR)/$$(DEVICE_DTS).dtb
+  DEVICE_TITLE := Methode uDPU Board
+  DEVICE_PACKAGES := f2fs-tools e2fsprogs fdisk ethtool kmod-usb2 kmod-usb3 kmod-e100 kmod-e1000 kmod-e1000e \
+			kmod-igb kmod-ixgbevf kmod-mdio-gpio kmod-sky2 kmod-switch-mvsw61xx kmod-bonding
+  IMAGES := sysupgrade.tar.gz
+  IMAGE_NAME := $$(IMAGE_PREFIX)-sysupgrade.tar.gz
+  IMAGE/sysupgrade.tar.gz := uDPU-firmware | gzip | append-metadata
+  DEVICE_DTS := uDPU
+  DTS_DIR := $(DTS_DIR)/marvell
+  SUPPORTED_DEVICES := methode,uDPU
+endef
+TARGET_DEVICES += uDPU
 
 endif
